792929cb3d007868f16aa215922e385c994b9680
Johnathan Lyman <jelyman@gmail.com>
2016-03-27 23:18:22 -0700

    Create Tasks model & display task list at /tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
   root 'welcome#index'
   get '/profile', to: 'users#show'
+
   devise_for :users, controllers: { registrations: 'registrations' }
+
+  resources :tasks
+
 end
