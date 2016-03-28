516c52baf05c5e6dcf410ccfe693ec52e5b36467
Johnathan Lyman <jelyman@gmail.com>
2016-03-27 23:33:19 -0700

    Add task creation functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,23 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new
+    @task.name = params[:task][:name]
+    @task.user_id = current_user.id
+
+    if @task.save
+      flash[:notice] = 'Task was saved.'
+      redirect_to tasks_path
+    else
+      flash.now[:alert] = 'There was an error saving the task. Please try again.'
+      render :new
+    end
+  end
+
 end
