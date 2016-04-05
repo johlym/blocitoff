2eb72fcfea380e6340b6763a8742b2c0471e6e0f
Johnathan Lyman <jelyman@gmail.com>
2016-04-04 17:57:31 -0700

    fix cap confi

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server 'your_server_ip', port: your_port_num, roles: [:web, :app, :db], primary: true
+server 'appsrv01.johnathanlyman.com', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:johlym/blocitoff'
 set :application,     'blocitoff'
