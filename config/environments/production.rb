bfd6e0c46b6ac519f35d13d6478c78e12b1e4df4
Johnathan Lyman <jelyman@gmail.com>
2016-04-08 15:44:00 -0700

    updates

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,7 @@ Rails.application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
+  config.logger = RemoteSyslogLogger.new('logs3.papertrailapp.com', 43111)
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
